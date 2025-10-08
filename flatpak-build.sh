4f6de9f7c8d9cd6d75041b9801c928008efd61c6
Michal Banas <mb.michal.banas@gmail.com>
2025-10-08 15:48:38 +0200

    build stepo added to script

--- flatpak-build.sh
+++ flatpak-build.sh
@@ -5,3 +5,4 @@ just vendor
 
 # Build the flatpak
 flatpak-builder --force-clean --jobs=1 -v build-dir com.github.digit1024.ms-todo-app.json
+flatpak build-bundle repo ms-todo-app.flatpak com.github.digit1024.ms-todo-app
